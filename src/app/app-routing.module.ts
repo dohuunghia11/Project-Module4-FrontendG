8a69590e0fdd9d43826f408176d17427b68e114d
dohuunghia11 <huunghiado1995@gmail.com>
2019-09-05 22:19:32 +0700

    add path: 'register'

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {SingInComponent} from './sing-in/sing-in.component';
+import {RegisterComponent} from './register/register.component';
 
 
 const routes: Routes = [
   {
     path: 'signIn', component: SingInComponent
+  },
+  {
+    path: 'register', component: RegisterComponent
   }
 ];
 @NgModule({
